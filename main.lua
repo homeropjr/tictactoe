41f5afd7b40af77317fcd40cd1b80be0507801c2
homero <homeropjr@gmail.com>
2022-06-21 23:49:53 -0300

    Add board hover

--- main.lua
+++ main.lua
@@ -18,39 +18,39 @@ GRID_COL_WIDTH = 4
 GRID_COL_HEIGHT = GRID_HEIGHT
 
 function love.load()
-    love.window.setTitle("Tic Tac Toe")
+    love.window.setTitle("Tic Tac Toe")    
 end
 
 function love.draw()    
     drawBoard()
-
-    local mousePosX, mousePosY = love.mouse.getPosition()
-    
-    if mousePosX > MARGIN_LEFT and mousePosX < MARGIN_LEFT + GRID_HEIGHT then
-    -- drawSquare(MARGIN_LEFT, MARGIN_TOP)
-    -- drawSquare(GRID_COL_X, MARGIN_TOP)
-    -- drawSquare(GRID_COL_XX, MARGIN_TOP)
-    
-
-    -- drawSquare(MARGIN_LEFT, GRID_ROW_Y)
-    -- drawSquare(GRID_COL_X, GRID_ROW_Y)
-    -- drawSquare(GRID_COL_XX, GRID_ROW_Y)
-
-    -- drawSquare(MARGIN_LEFT, GRID_ROW_YY)
-    -- drawSquare(GRID_COL_X, GRID_ROW_YY)
-    -- drawSquare(GRID_COL_XX, GRID_ROW_YY)
- end
+    drawBoardHover()    
 end
 
 function drawBoard()
     drawBoxes()
     drawGrid()
+
+    drawSquare(MARGIN_LEFT, MARGIN_TOP)
+    drawSquare(GRID_COL_X, MARGIN_TOP)
+    drawSquare(GRID_COL_XX, MARGIN_TOP)
+    
+    drawSquare(MARGIN_LEFT, GRID_ROW_Y)
+    drawSquare(GRID_COL_X, GRID_ROW_Y)
+    drawSquare(GRID_COL_XX, GRID_ROW_Y)
+    
+    drawSquare(MARGIN_LEFT, GRID_ROW_YY)
+    drawSquare(GRID_COL_X, GRID_ROW_YY)
+    drawSquare(GRID_COL_XX, GRID_ROW_YY)
  end
 
-function drawSquare(x, y)
-    love.graphics.setColor(love.math.colorFromBytes(41, 54, 61))
-    love.graphics.rectangle("fill", x + 7, y + 7, 140, 140, 5, 5)
-end
+function drawBoxes()
+    local borderRadius = 10
+    love.graphics.setColor(love.math.colorFromBytes(34, 40, 42))
+    love.graphics.rectangle("fill", 40, 80, 620, 500, borderRadius, borderRadius)
+    
+    love.graphics.rectangle("fill", 40, 10, 300, 60, borderRadius, borderRadius)
+    love.graphics.rectangle("fill", 360, 10, 300, 60, borderRadius, borderRadius)
+end 
 
 function drawGrid()
     love.graphics.setColor(255, 255, 255)
@@ -62,11 +62,58 @@ function drawGrid()
     love.graphics.rectangle("fill", GRID_COL_XX, GRID_COL_Y, GRID_COL_WIDTH, GRID_COL_HEIGHT)
 end
 
-function drawBoxes()
-    local borderRadius = 10
-    love.graphics.setColor(love.math.colorFromBytes(34, 40, 42))
-    love.graphics.rectangle("fill", 40, 80, 620, 500, borderRadius, borderRadius)
+function drawSquare(x, y)
+    love.graphics.setColor(love.math.colorFromBytes(41, 54, 61))
+    love.graphics.rectangle("fill", x + 7, y + 7, 140, 140, 5, 5)
+end
+
+function drawBoardHover()
+    local mousePosX, mousePosY = love.mouse.getPosition()
     
-    love.graphics.rectangle("fill", 40, 10, 300, 60, borderRadius, borderRadius)
-    love.graphics.rectangle("fill", 360, 10, 300, 60, borderRadius, borderRadius)
-end 
+    if mousePosX > MARGIN_LEFT and mousePosX < GRID_COL_XX + 148 then
+
+        if mousePosY > MARGIN_TOP and mousePosY < GRID_ROW_Y then
+
+            if mousePosX > MARGIN_LEFT and mousePosX < GRID_COL_X then
+                drawSquareHover(MARGIN_LEFT, MARGIN_TOP)
+
+            elseif mousePosX > GRID_COL_X and mousePosX < GRID_COL_XX then
+                drawSquareHover(GRID_COL_X, MARGIN_TOP)
+
+            elseif mousePosX > GRID_COL_XX and  mousePosX < GRID_COL_XX + GRID_HEIGHT then
+                drawSquareHover(GRID_COL_XX, MARGIN_TOP)
+
+            end
+
+        elseif mousePosY >  GRID_ROW_Y and mousePosY < GRID_ROW_YY then
+
+            if mousePosX > MARGIN_LEFT and mousePosX < GRID_COL_X then
+                drawSquareHover(MARGIN_LEFT, GRID_ROW_Y)        
+
+            elseif mousePosX > GRID_COL_X and mousePosX < GRID_COL_XX then
+                drawSquareHover(GRID_COL_X, GRID_ROW_Y)        
+
+            elseif mousePosX > GRID_COL_XX and  mousePosX < GRID_COL_XX + GRID_HEIGHT then
+                drawSquareHover(GRID_COL_XX, GRID_ROW_Y)
+
+            end
+
+        elseif mousePosY >  GRID_ROW_YY and mousePosY < GRID_ROW_YY + 148 then
+
+            if mousePosX > MARGIN_LEFT and mousePosX < GRID_COL_X then
+                drawSquareHover(MARGIN_LEFT, GRID_ROW_YY)
+
+            elseif mousePosX > GRID_COL_X and mousePosX < GRID_COL_XX then
+                drawSquareHover(GRID_COL_X, GRID_ROW_YY)
+
+            elseif mousePosX > GRID_COL_XX and  mousePosX < GRID_COL_XX + GRID_HEIGHT then
+                  drawSquareHover(GRID_COL_XX, GRID_ROW_YY)
+            end
+        end
+    end
+end
+
+function drawSquareHover(x, y)
+    love.graphics.setColor(love.math.colorFromBytes(123,94,198))
+    love.graphics.rectangle("fill", x + 7, y + 7, 140, 140, 5, 5)
+end
